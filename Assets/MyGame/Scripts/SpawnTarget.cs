06f5f71a0ef56e09d98401eff64d1b1e719e2372
Eva Innerlohninger <evainnerlohninger@Eva-M1.local>
2021-12-02 14:53:32 +0100

    create unity file

---
+++ Assets/MyGame/Scripts/SpawnTarget.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnTarget : MonoBehaviour
+{
+    public GameObject target;
+    public Text score;
+    public Text won;
+
+
+
+public void OnMouseKlick()
+{
+
+        if
+
+}
+}
+//if shot neue position random vom bild else wait  sec und randomize popsition
